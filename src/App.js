9c3b2f8a563852f10b4c610f7951b000ad185533
JrKaio <kaiojunio22@hotmail.com>
2023-12-19 15:13:01 -0300

    descrição breve do commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Nomes from './Nomes';
+import { useState } from 'react';
+//No Nomes a gente chamou a página de nomes, no aluno colocou nome e a idade, a idade 
+//Se colocar as variáveis chamando diferente do que foi aplicado na outra página, não funcionará
+// Idade = Com I Maiúsculo não funciona porque ele foi definido com letra mínuscula
+const App = () => { // Funçao
+  const [aluno, setAluno] = useState('Sujeito Programador') // começa com esse nome
+  const [count, setCount] = useState(0); // Começou com 0
+  function handleChangeName(){
+    setAluno('Matheus') // setAluno é pra mudar o nome
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Componente App</h1>
+      <br/>
+      <h2>Olá: {aluno}</h2>
+      <button onClick={handleChangeName}>
+        Mudar nome
+      </button>
+    <br/>
+      <Nomes aluno="Lucas" idade= {24} />
+      
+      <br/>
+      <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
-  );
+      
+
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Nomes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+//Criamos uma função que vai exibir  o nome do aluno e a idade
+
+const Nomes = ({aluno, idade}) => {
+  return (
+    <spam>Bem-vindo: {aluno} - Idade {idade} anos </spam>
+  )
+}
+
+export default Nomes
